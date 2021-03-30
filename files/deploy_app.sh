3252a149dcbe792ebf709661a9b7b23b618a4b7a
Liam <liamdonnelly.nyc@gmail.com>
2021-03-30 15:31:33 +0000

    deploy new image

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -9,7 +9,7 @@ cat << EOM > /var/www/html/index.html
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
-  <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
+  <center><img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fgiphy.com%2Fexplore%2Flame-party&psig=AOvVaw3zLEm6GGGTQOUsJmGXeXGX&ust=1617204636634000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMDe0_Kq2O8CFQAAAAAdAAAAABAJ"></img></center>
   <center><h2>Meow World!</h2></center>
   Welcome to ${PREFIX}'s app. Where the only thing bigger than our brains.. is our hearts.
   <!-- END -->
